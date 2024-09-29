5347c44b44aa42c9c65216f90f079446b986a390
sodam <damsoshin.dev@gmail.com>
2024-09-29 14:24:15 +0900

    chore: local font 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
-import './globals.css';
+import localFont from 'next/font/local';
 import { ReactNode } from 'react';
+import './globals.css';
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html data-theme="cmyk" lang="ko">
+    <html data-theme="cmyk" lang="ko" className={pretendard.className}>
       <body className="min-h-screen bg-gray-100 text-gray-900">
         <div className="mx-auto min-h-screen w-full max-w-[600px] bg-white p-4">
           <main>{children}</main>
