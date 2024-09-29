c8b0188ee238eb0b2d248b6d02a71098ff6d536d
sodam <damsoshin.dev@gmail.com>
2024-09-29 21:59:18 +0900

    feat: Home 페이지 loading UI 추가

---
+++ src/_components/LoadingDots.tsx
@@ -0,0 +1,8 @@
+export default function LoadingDots() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <span className="loading loading-dots loading-lg"></span>
+      <span className="ml-2">Loading...</span>
+    </div>
+  );
+}
---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,5 @@
+import LoadingDots from '@/_components/LoadingDots';
+
+export default function Loading() {
+  return <LoadingDots />;
+}
