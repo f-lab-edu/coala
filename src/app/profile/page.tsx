f1832d5c95ed9bb165f839b2c3dabd7fba2ac38b
sodam <damsoshin.dev@gmail.com>
2024-10-06 23:45:07 +0900

    feat: 프로필 페이지 UI 1차 마크업

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,31 @@
+import UserInfo from '../(home)/_components/UserInfo';
+
 export default function Page() {
-  return <h1>Profile</h1>;
+  return (
+    <div className="mx-auto max-w-[600px] p-4">
+      {/* 프로필 섹션 */}
+      <UserInfo />
+
+      <section className="mb-6">
+        <h2 className="mb-4 text-2xl font-bold">자산 정보</h2>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="rounded bg-base-100 p-4 shadow">
+            <h3 className="text-lg font-semibold">총 자산</h3>
+            <p className="text-gray-700">₩10,000,000</p>
+          </div>
+          <div className="rounded bg-base-100 p-4 shadow">
+            <h3 className="text-lg font-semibold">총 손익</h3>
+            <p className="text-gray-700">₩500,000 (5%)</p>
+          </div>
+        </div>
+      </section>
+
+      {/* 설정 섹션 */}
+      <section>
+        <div className="space-y-4">
+          <button className="btn no-animation btn-block">로그아웃</button>
+        </div>
+      </section>
+    </div>
+  );
 }
