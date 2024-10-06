d27933fae3b6a98e7dd6e9b83ba5e325ce7db775
sodam <damsoshin.dev@gmail.com>
2024-10-06 23:33:03 +0900

    login-success 리팩토링

--- src/app/api/auth/login-success/route.ts
+++ src/app/api/auth/login-success/route.ts
@@ -9,35 +9,45 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   try {
-    const searchParams = parseSearchParams(request.nextUrl.searchParams);
-    const code = searchParams.code || '';
-    if (code === '') {
-      return NextResponse.json({ message: 'code is empty' }, { status: 400 });
-    }
+    const code = await getGoogleAuthCode(request);
     const profile = await getGoogleUserProfile(code);
-
-    const { id: providerId, email: providerEmail, picture: providerPicture } = profile;
-
-    const [result] = (await sql`
-      INSERT INTO users (email, google_id, picture)
-      VALUES (${providerEmail}, ${providerId}, ${providerPicture})
-      ON CONFLICT (email) 
-      DO UPDATE SET
-        google_id = EXCLUDED.google_id,
-        picture = EXCLUDED.picture,
-        email = EXCLUDED.email
-      RETURNING *
-    `) as Array<{ id: string; email: string }>;
-    if (result == null) {
-      return NextResponse.json({ message: 'handle_google_auth failed' }, { status: 400 });
-    }
-    const user_id = Number(result.id);
-
-    const token = await signToken({ id: user_id });
-    setAuthCookie(token);
-
+    const userId = await insertOrUpdateUser(profile);
+    await generateAndSetAuthToken(userId);
     return NextResponse.redirect(`${APP_DOMAIN}`);
   } catch (e) {
     serverErrorHandler(e, 'Error during Google OAuth login process');
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
   }
+}
+
+async function getGoogleAuthCode(request: NextRequest) {
+  const searchParams = parseSearchParams(request.nextUrl.searchParams);
+  const code = searchParams.code || '';
+  if (code === '') {
+    throw new Error('Code is empty');
+  }
+  return code;
+}
+
+async function insertOrUpdateUser(profile: { id: string; email: string; picture: string }) {
+  const { id: providerId, email: providerEmail, picture: providerPicture } = profile;
+  const [result] = (await sql`
+    INSERT INTO users (email, google_id, picture)
+    VALUES (${providerEmail}, ${providerId}, ${providerPicture})
+    ON CONFLICT (email) 
+    DO UPDATE SET
+      google_id = EXCLUDED.google_id,
+      picture = EXCLUDED.picture,
+      email = EXCLUDED.email
+    RETURNING *
+  `) as Array<{ id: string; email: string }>;
+  if (result == null) {
+    throw new Error('handle_google_auth failed');
+  }
+  return Number(result.id);
+}
+
+async function generateAndSetAuthToken(userId: number) {
+  const token = await signToken({ id: userId });
+  setAuthCookie(token);
 }
