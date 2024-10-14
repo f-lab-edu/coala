2259d888d96eee5dde07607e5b63b404c868c038
sodam-run-dev <157378788+sodam-run-dev@users.noreply.github.com>
2024-10-14 06:16:03 +0000

    react-query 셋팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import localFont from 'next/font/local';
 import { ReactNode } from 'react';
 import './globals.css';
 import BottomNav from '@/_components/BottomNav';
+import ReactQueryProvider from '@/_providers/ReactQueryProvider';
 
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
@@ -13,10 +14,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html data-theme="cmyk" lang="ko" className={pretendard.className}>
       <body className="min-h-screen bg-gray-100 text-gray-900">
+        <ReactQueryProvider>
         <div className="mx-auto min-h-screen w-full max-w-[600px] bg-white p-4">
           <main>{children}</main>
           <BottomNav />
         </div>
+        </ReactQueryProvider>
       </body>
     </html>
   );
